f2656d6b9fd18293744c11def139376cfc031974
fabiohj7 <fabiohj7@gmail.com>
2024-01-13 19:54:38 -0400

    Added skills

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule
